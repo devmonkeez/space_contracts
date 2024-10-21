df54c0afb8f51190a6f7e728a595f7cf4d37b2a2
uroshinjo <hrastar.uros@gmail.com>
2024-10-21 10:23:18 +0200

    COL-01 | Inaccurate Cost Due To Different Token Price

--- SpacemNodes.sol
+++ SpacemNodes.sol
@@ -121,7 +121,7 @@ contract SpacemNodes is Ownable(msg.sender), ReentrancyGuard {
         usdtToken = IERC20(0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7);
         rewardsToken = IERC20(0x3c780F5cBF94De3EFCec964Af928D08c4508EeBE);
         collection = Collection(_collection);
-        
+
         stakingAddress = _stakingAddress;
     }
 
@@ -274,7 +274,7 @@ contract SpacemNodes is Ownable(msg.sender), ReentrancyGuard {
         }
     }
 
-    function getPrice(uint256 tokenId) public pure returns (uint256) {
+    function getPrice(uint256 tokenId) private pure returns (uint256) {
         if(tokenId < 500) { 
             return 300 * 1e6;
         } else if(tokenId >= 500 && tokenId < 2000) {
